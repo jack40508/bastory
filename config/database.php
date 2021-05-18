a00f1e7cec18031089f6115b379706bd82d65027
YoYu <jack40508@gmail.com>
2021-05-18 17:34:59 +0900

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Str;
 
-return [
+$db_config = get_db_config();
 
-    'default' => $db_config = get_db_config();
+return [
 
     /*
     |--------------------------------------------------------------------------
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => $db_config['connection'],
 
     /*
     |--------------------------------------------------------------------------
